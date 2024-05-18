8f2b0e5b7cf89420870478b900f42d37b9d0fb46
Emaanwilson <158086429+nightxcl0ud@users.noreply.github.com>
2024-05-18 09:18:31 -0600

    bebug complete  \run verseion config

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_login import LoginManager
 from .config import Config
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
