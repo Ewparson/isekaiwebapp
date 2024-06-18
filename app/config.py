fe727f4d2d401b86ddf6a3d289b212b9026f330b
Emaanwilson <158086429+nightxcl0ud@users.noreply.github.com>
2024-06-18 06:49:40 -0600

    Update config.py with environment variable loading

--- app/config.py
+++ app/config.py
@@ -1,7 +1,5 @@
+from dotenv import load_dotenv
 # config.py
 import os
+load_dotenv()
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'HEqzrcjFMW5v'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://Nightcloud:09Pkb44wbqw!@localhost:3306/serenity_studio_database'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
